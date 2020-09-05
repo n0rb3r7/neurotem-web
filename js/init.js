62f80029494c10f84050d56e573404f175f48b93
n0rb3r7 <n0rb3r7@outlook.com>
2020-09-05 18:27:16 -0300

    General update
    Owl carousel added

--- js/init.js
+++ js/init.js
@@ -1,8 +1,26 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
+    $('.collapsible').collapsible();
+    $('.owl-carousel').owlCarousel({
+      items:3,
+      merge:true,
+      loop:true,
+      margin:30,
+      video:true,
+      lazyLoad:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:8
+          }
+      }
+  })
   }); // end of document ready
 })(jQuery); // end of jQuery name space
